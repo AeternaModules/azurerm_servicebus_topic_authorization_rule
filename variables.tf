574a312b7fe11f1c2fe4b68c2c137cad94294404
Daniel Allen <dan@aeternagroup.net>
2026-07-13 19:37:57 +0100

    Update to schema version v4.80.0

--- variables.tf
+++ variables.tf
@@ -13,9 +13,9 @@ EOT
   type = map(object({
     name     = string
     topic_id = string
-    listen   = optional(bool) # Default: false
-    manage   = optional(bool) # Default: false
-    send     = optional(bool) # Default: false
+    listen   = optional(bool)
+    manage   = optional(bool)
+    send     = optional(bool)
   }))
 }
 
