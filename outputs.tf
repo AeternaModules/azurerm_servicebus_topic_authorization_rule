2cdc0c4ac6fc150ea8177a5457b90221b5e17d51
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:50:52 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,55 +1,55 @@
 output "servicebus_topic_authorization_rules_id" {
   description = "Map of id values across all servicebus_topic_authorization_rules, keyed the same as var.servicebus_topic_authorization_rules"
-  value       = { for k, v in azurerm_servicebus_topic_authorization_rule.servicebus_topic_authorization_rules : k => v.id }
+  value       = { for k, v in azurerm_servicebus_topic_authorization_rule.servicebus_topic_authorization_rules : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "servicebus_topic_authorization_rules_listen" {
   description = "Map of listen values across all servicebus_topic_authorization_rules, keyed the same as var.servicebus_topic_authorization_rules"
-  value       = { for k, v in azurerm_servicebus_topic_authorization_rule.servicebus_topic_authorization_rules : k => v.listen }
+  value       = { for k, v in azurerm_servicebus_topic_authorization_rule.servicebus_topic_authorization_rules : k => v.listen if v.listen != null }
 }
 output "servicebus_topic_authorization_rules_manage" {
   description = "Map of manage values across all servicebus_topic_authorization_rules, keyed the same as var.servicebus_topic_authorization_rules"
-  value       = { for k, v in azurerm_servicebus_topic_authorization_rule.servicebus_topic_authorization_rules : k => v.manage }
+  value       = { for k, v in azurerm_servicebus_topic_authorization_rule.servicebus_topic_authorization_rules : k => v.manage if v.manage != null }
 }
 output "servicebus_topic_authorization_rules_name" {
   description = "Map of name values across all servicebus_topic_authorization_rules, keyed the same as var.servicebus_topic_authorization_rules"
-  value       = { for k, v in azurerm_servicebus_topic_authorization_rule.servicebus_topic_authorization_rules : k => v.name }
+  value       = { for k, v in azurerm_servicebus_topic_authorization_rule.servicebus_topic_authorization_rules : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "servicebus_topic_authorization_rules_primary_connection_string" {
   description = "Map of primary_connection_string values across all servicebus_topic_authorization_rules, keyed the same as var.servicebus_topic_authorization_rules"
-  value       = { for k, v in azurerm_servicebus_topic_authorization_rule.servicebus_topic_authorization_rules : k => v.primary_connection_string }
+  value       = { for k, v in azurerm_servicebus_topic_authorization_rule.servicebus_topic_authorization_rules : k => v.primary_connection_string if v.primary_connection_string != null && length(v.primary_connection_string) > 0 }
   sensitive   = true
 }
 output "servicebus_topic_authorization_rules_primary_connection_string_alias" {
   description = "Map of primary_connection_string_alias values across all servicebus_topic_authorization_rules, keyed the same as var.servicebus_topic_authorization_rules"
-  value       = { for k, v in azurerm_servicebus_topic_authorization_rule.servicebus_topic_authorization_rules : k => v.primary_connection_string_alias }
+  value       = { for k, v in azurerm_servicebus_topic_authorization_rule.servicebus_topic_authorization_rules : k => v.primary_connection_string_alias if v.primary_connection_string_alias != null && length(v.primary_connection_string_alias) > 0 }
   sensitive   = true
 }
 output "servicebus_topic_authorization_rules_primary_key" {
   description = "Map of primary_key values across all servicebus_topic_authorization_rules, keyed the same as var.servicebus_topic_authorization_rules"
-  value       = { for k, v in azurerm_servicebus_topic_authorization_rule.servicebus_topic_authorization_rules : k => v.primary_key }
+  value       = { for k, v in azurerm_servicebus_topic_authorization_rule.servicebus_topic_authorization_rules : k => v.primary_key if v.primary_key != null && length(v.primary_key) > 0 }
   sensitive   = true
 }
 output "servicebus_topic_authorization_rules_secondary_connection_string" {
   description = "Map of secondary_connection_string values across all servicebus_topic_authorization_rules, keyed the same as var.servicebus_topic_authorization_rules"
-  value       = { for k, v in azurerm_servicebus_topic_authorization_rule.servicebus_topic_authorization_rules : k => v.secondary_connection_string }
+  value       = { for k, v in azurerm_servicebus_topic_authorization_rule.servicebus_topic_authorization_rules : k => v.secondary_connection_string if v.secondary_connection_string != null && length(v.secondary_connection_string) > 0 }
   sensitive   = true
 }
 output "servicebus_topic_authorization_rules_secondary_connection_string_alias" {
   description = "Map of secondary_connection_string_alias values across all servicebus_topic_authorization_rules, keyed the same as var.servicebus_topic_authorization_rules"
-  value       = { for k, v in azurerm_servicebus_topic_authorization_rule.servicebus_topic_authorization_rules : k => v.secondary_connection_string_alias }
+  value       = { for k, v in azurerm_servicebus_topic_authorization_rule.servicebus_topic_authorization_rules : k => v.secondary_connection_string_alias if v.secondary_connection_string_alias != null && length(v.secondary_connection_string_alias) > 0 }
   sensitive   = true
 }
 output "servicebus_topic_authorization_rules_secondary_key" {
   description = "Map of secondary_key values across all servicebus_topic_authorization_rules, keyed the same as var.servicebus_topic_authorization_rules"
-  value       = { for k, v in azurerm_servicebus_topic_authorization_rule.servicebus_topic_authorization_rules : k => v.secondary_key }
+  value       = { for k, v in azurerm_servicebus_topic_authorization_rule.servicebus_topic_authorization_rules : k => v.secondary_key if v.secondary_key != null && length(v.secondary_key) > 0 }
   sensitive   = true
 }
 output "servicebus_topic_authorization_rules_send" {
   description = "Map of send values across all servicebus_topic_authorization_rules, keyed the same as var.servicebus_topic_authorization_rules"
-  value       = { for k, v in azurerm_servicebus_topic_authorization_rule.servicebus_topic_authorization_rules : k => v.send }
+  value       = { for k, v in azurerm_servicebus_topic_authorization_rule.servicebus_topic_authorization_rules : k => v.send if v.send != null }
 }
 output "servicebus_topic_authorization_rules_topic_id" {
   description = "Map of topic_id values across all servicebus_topic_authorization_rules, keyed the same as var.servicebus_topic_authorization_rules"
-  value       = { for k, v in azurerm_servicebus_topic_authorization_rule.servicebus_topic_authorization_rules : k => v.topic_id }
+  value       = { for k, v in azurerm_servicebus_topic_authorization_rule.servicebus_topic_authorization_rules : k => v.topic_id if v.topic_id != null && length(v.topic_id) > 0 }
 }
 
